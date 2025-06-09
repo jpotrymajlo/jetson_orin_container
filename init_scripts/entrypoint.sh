6059cc6595c4223d0beddf5d9a9fddaa94e07d36
Jacek Potrymajlo <j.potrymajlo@gmail.com>
2025-06-09 22:47:42 +0200

    Added conan2 and managing ssh keys

--- init_scripts/entrypoint.sh
+++ init_scripts/entrypoint.sh
@@ -1,6 +1,6 @@
 
 
-set -e -x
+#set -e -x
 
 echo "start container"
 
@@ -19,12 +19,27 @@ sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication no/' /etc/ssh/sshd_conf
 sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config && \
 sed -i 's/UsePAM yes/UsePAM no/' /etc/ssh/sshd_config && \
 echo "AllowUsers ${DOCKER_USER}" >> /etc/ssh/sshd_config
+
+#start sshd
+
+key_storage=${DOCKER_HOME}/.ssh/docker_keys
+if [ ! -d ${key_storage} ]; then
+    mkdir -p ${key_storage}
+fi
+
+if [ ! -f ${key_storage}/ed25519_host_key ]; then
+    /usr/bin/ssh-keygen -t ed25519 -f ${key_storage}/ed25519_host_key
+fi
+
+cp ${key_storage}/* /etc/ssh
+
+
 mkdir -p /run/sshd
 
 sudo /usr/sbin/sshd
 
 source /etc/profile
 cd $HOME
-
+export PATH=$PATH:${DOCKER_HOME}/.local/bin
 exec runuser -u "${DOCKER_USER}" -- bash
 
--- init_scripts/init.sh
+++ init_scripts/init.sh
@@ -5,7 +5,6 @@ set -e -x
 apt-get update && \
     apt-get install -y --no-install-recommends \
     git \
-    cmake \
     sudo \
     vim \
     vim-addon-manager \
@@ -13,6 +12,15 @@ apt-get update && \
     curl \
     wget \
     ssh \
+    python3-pip \
     && rm -rf /var/lib/apt/lists/*
 
+wget https://github.com/Kitware/CMake/releases/download/v4.0.2/cmake-4.0.2-linux-aarch64.sh \
+    -q -O /tmp/cmake-install.sh && \
+    chmod u+x /tmp/cmake-install.sh && \
+    mkdir /opt/cmake-4.0.2 && \
+    /tmp/cmake-install.sh --skip-license --prefix=/opt/cmake-4.0.2 && \
+    rm /tmp/cmake-install.sh && \
+    ln -s /opt/cmake-4.0.2/bin/* /usr/local/bin
 
+pip install conan
